ee0e79ceb3b2ddbf9764ba3854f4def89290d713
Ryszard Pyka <ryszard@pyka.co.uk>
2021-08-17 12:54:29 +0100

    Add project files.

---
+++ Test/Faro Shuffle.cs
@@ -0,0 +1,114 @@
+﻿namespace FaroShuffle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            Console.WriteLine("Starting deck:");
+
+            // Display each card that we've generated and placed in startingDeck in the console
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+
+            Console.WriteLine("\nShuffled deck:");
+
+
+
+            var times = 0;
+            // We can re-use the shuffle variable from earlier, or you can make a new one
+            var shuffle = startingDeck;
+            do
+            {
+                shuffle = shuffle.Skip(26).InterleaveSequenceWith(shuffle.Take(26));
+
+                foreach (var card in shuffle)
+                {
+                    Console.WriteLine(card);
+                }
+                Console.WriteLine("\n" + times);
+                times++;
+
+            } while (!startingDeck.SequenceEquals(shuffle));
+
+            Console.WriteLine(times);
+        }
+
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+
+        public static bool SequenceEquals<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static IEnumerable<T> LogQuery<T>(this IEnumerable<T> sequence, string tag)
+        {
+            // File.AppendText creates a new file if the file doesn't exist.
+            using (var writer = File.AppendText("debug.log"))
+            {
+                writer.WriteLine($"Executing Query {tag}");
+            }
+
+            return sequence;
+        }
+    }
+}
---
+++ Test/Switch Example.cs
@@ -0,0 +1,66 @@
+﻿namespace SwitchExample
+{
+    using System;
+
+    class Switch
+    {
+        public static void Main1()
+        {
+            var t = new Test();
+            try
+            {
+                Console.WriteLine(t.Sprawdz("2"));
+                Console.WriteLine($"{t.CalculateDiscount(new Order(20, 5000)) * 100:0.0}%");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+
+    class Test
+    {
+        public string Sprawdz(string t) =>
+            t switch
+            {
+                "1" => "We have 1.",
+                "2" => "We have 2.",
+                "3" => "We have 3.",
+                _ => throw new ArgumentException("Invalid string number.", nameof(t)),
+            };
+
+        string WaterState(int tempInFahrenheit) =>
+            tempInFahrenheit switch
+            {
+                (> 32) and (< 212) => "liquid",
+                < 32 => "solid",
+                > 212 => "gas",
+                32 => "solid/liquid transition",
+                212 => "liquid / gas transition",
+            };
+
+        string WaterState2(int tempInFahrenheit) =>
+            tempInFahrenheit switch
+            {
+                < 32 => "solid",
+                32 => "solid/liquid transition",
+                < 212 => "liquid",
+                212 => "liquid / gas transition",
+                _ => "gas",
+            };
+
+        public decimal CalculateDiscount(Order order) =>
+            order switch
+            {
+                { Items: > 10, Cost: > 1000.00m } => 0.10m,
+                (Items: > 5, Cost: > 500.00m) => 0.05m,
+                { Cost: > 250.00m } => 0.02m,
+                null => throw new ArgumentNullException(nameof(order), "Can't calculate discount on null order"),
+                _ => 0m,
+            };
+    }
+
+    public record Order(int Items, decimal Cost);
+}
